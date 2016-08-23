7c53db9e6a5623f9b74198068d139d1022e399d6
Steeveta <Steeveta@gmail.com>
2016-08-23 15:27:04 -0500

    Primera Publicacion
    Versión Alpha

---
+++ views/pages/index.php
@@ -0,0 +1,18 @@
+<div class="row">
+      <div class="col s12 m5">
+        <ul class="collapsible" data-collapsible="accordion">
+     <li>
+       <div class="collapsible-header"><i class="material-icons">filter_drama</i>Mensaje del sistema!! </div>
+       <div class="collapsible-body"><p><?php if (isset($_SESSION['username'])): ?>Bienvenido <?php echo $_SESSION['username']; ?>!  <?php endif; ?></p></div>
+     </li>
+     <li>
+       <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+       <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+     </li>
+     <li>
+       <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+       <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+     </li>
+   </ul>
+      </div>
+    </div>
